65083d5eb1a9061c2b406865f80085b38cde7103
Vitalii <v.levitskyi@mettevo.org>
2025-02-15 23:36:00 +0200

    fix bugs

--- page.php
+++ page.php
@@ -4,9 +4,7 @@
 
 <div class="main">
     <div class="page__wrapper container">
-        <h1>
-            <?php the_title(); ?>
-        </h1>
+       
         <div class="page__content h-content">
             <?php the_content(); ?>
         </div>
